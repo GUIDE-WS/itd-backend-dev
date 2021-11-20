d3c4123b512a5f6417d533fedb26adbb5a90e21e
angst-storm <Sergey.Kiprin@urfu.me>
2021-11-21 01:46:05 +0500

    апи готово в целом бля бля

--- restapi/api/urls.py
+++ restapi/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import ServiceList
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', ServiceList.as_view())
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = format_suffix_patterns(urlpatterns)
